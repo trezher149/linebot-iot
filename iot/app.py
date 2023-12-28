b39ebd0927b83a8cbd4975a1bbd17477c7f12bf0
nattanol boonyakhet <6310682726@student.tu.ac.th>
2023-12-28 13:14:09 +0700

    commit for pull

--- iot/app.py
+++ iot/app.py
@@ -18,27 +18,25 @@ mqtt.init_app(app)
 
 usrname = os.environ.get("MONGO_INITDB_ROOT_USERNAME")
 passwd = os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
-mongo_client = MongoClient(f"mongodb://{usrname}:{passwd}@linebot-iot-mongodb-1:27017/")
+environmen_database = MongoClient(f"mongodb://{usrname}:{passwd}@linebot-iot-mongodb-1:27017/")
 
 @mqtt.on_connect()
 def connect(client, flags, rc, properties):
-    mqtt.client.subscribe("cn/bigproj/device/1") #subscribing mqtt topic
+    mqtt.client.subscribe("cn/bigproj/setting/device/c466") #subscribing mqtt topic
     print("Connected: ", client, flags, rc, properties)
 
 @mqtt.on_message()
 async def message(client, topic, payload, qos, properties):
     print("Received message: ",topic, payload.decode())
-    db = mongo_client.test_database
-    collection = db.test_collection
+    collection = environmen_database.env_device_data
     data = json.loads(payload.decode())
-    post = { "topic": topic,"timestamp": datetime.now(), "humidity": data["humidity"], "temperature": data["temperature"]}
+    post = { "device_id": topic[4],"timestamp": datetime.now(), "humidity": data["humid"], "temperature": data["temp"], "pressure": data["pressure"]}
     collection.insert_one(post)
     return 0
 
 @app.get("/")
 async def func():
-    db = mongo_client.test_database
-    #collection = db.test_collection
-    #post = collection.find_one(sort= [( '_id', pymongo.DESCENDING)])
-    #post.pop("_id")
-    return "OK"
+    collection = environmen_database.env_device_data
+    post = collection.find_one(sort= [( '_id', pymongo.DESCENDING)])
+    post.pop("_id")
+    return post
