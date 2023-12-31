77f932a4f9083b435c33ab0dcf1f0b92ca9084fc
Munin Wutthipongworakit <6310682601@student.tu.ac.th>
2023-12-31 11:10:49 +0700

    add style sheet for html

--- liff/app.py
+++ liff/app.py
@@ -9,8 +9,8 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 client = MongoClient("mongodb://mongodb:27017/")
-db = client["database_name"] # edit database name
-collection = db["collection_name"] # edit collection name
+db = client["test_database"] #database name
+collection = db["env_device_data"] #collection name
 
 @app.get("/test")
 async def test_endpoint():
@@ -19,5 +19,5 @@ async def test_endpoint():
 @app.get("/", response_class=HTMLResponse)
 async def liff_html(request: Request):
     latest_entries = collection.find().sort("_id", -1).limit(15)
-    data_to_render = [{"field1": entry["field1"], "field2": entry["field2"]} for entry in latest_entries]
+    data_to_render = [{"field1": entry["field1"], "field2": entry["field2"], "field3": entry["field3"]} for entry in latest_entries]
     return templates.TemplateResponse("index.html", context={"request": request, "data": data_to_render})
