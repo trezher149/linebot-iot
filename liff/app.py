b4e364434abaf511e945722249ec44ac1b0dd7e7
Thunyavat Thanakornsombat <thunyavat.comEngr@hotmail.com>
2023-12-31 15:47:34 +0700

    change request method and add requests module

--- liff/app.py
+++ liff/app.py
@@ -2,19 +2,16 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-import pymongo
-from pymongo import MongoClient
-import os
-
-mongo_host = os.environ["MONGO_HOST"]
-usrname = os.environ["MONGO_INITDB_ROOT_USERNAME"]
-passwd = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
-mongo_client = MongoClient(f"mongodb://{usrname}:{passwd}@{mongo_host}")
+import requests
+#from pymongo import MongoClient
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+#client = MongoClient("mongodb://mongodb:27017/")
+#db = client["test_database"] #database name
+#collection = db["env_device_data"] #collection name
 
 @app.get("/test")
 async def test_endpoint():
@@ -22,11 +19,5 @@ async def test_endpoint():
 
 @app.get("/", response_class=HTMLResponse)
 async def liff_html(request: Request):
-    data = get_iot_data()
+    data = requests.post("http://iot:8000/api/front/environ_data", json={"device_id": "c466"})
     return templates.TemplateResponse("index.html", context={"request": request, "data": data})
-
-def get_iot_data():
-    db = mongo_client.test_database
-    collection = db.test_collection
-    data = collection.find({}, sort=[('_id', pymongo.DESCENDING)], limit=15)
-    return list(data)
